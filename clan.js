15adbadafcfaf109abe0202542b032bace101f8a
adikus <andrej.hoos@gmail.com>
2014-08-12 00:30:15 +0200

    New player updater

--- clan.js
+++ clan.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose'),
 module.exports = Clan = cls.Class.extend({
 	init: function(wid){
 		this.wid = parseInt(wid);
-		this.memberIds = [];
+		this.members = {};
 		this.done = 0;
 	},
 	
@@ -36,17 +36,11 @@ module.exports = Clan = cls.Class.extend({
 	        var clanData = JSON.parse(data);
             clanData.data = clanData.data[this.wid];
 	    }catch(e){
-	        //console.log(e);
 	        return false;
 	    }
 		
 		if(clanData.status == "error" || !clanData.data){
 			this.doc.s = clanData.error || 'Unknown error';
-			/*this.doc.t = "";
-			this.doc.n = "";
-			this.doc.m = "";
-			this.doc.d = "";
-			this.members_count = 0;*/
 		}
 		else {
 			this.doc.t = clanData.data.abbreviation;
@@ -55,89 +49,57 @@ module.exports = Clan = cls.Class.extend({
 			this.doc.m = clanData.data.motto;
 			this.doc.d = clanData.data.description_html;
 			this.members_count = clanData.data.members_count;
-			for(i in clanData.data.members){
-				pid = clanData.data.members[i].account_id;
-				this.memberIds.push(pid);
+			for(var i in clanData.data.members){
+				var pid = clanData.data.members[i].account_id;
+				this.members[pid] = clanData.data.members[i].account_name;
 			}
-			this.memberIds = _.map(this.memberIds,function(wid){
-				return parseInt(wid);
+
+			var ids = _(_(this.members).keys()).map(function(val){return parseInt(val, 10)});
+
+			DBTypes.Player.find({_id: {$in: ids}}, function(err, players) {
+				if(players){
+					var found = {};
+					_(players).each(function(player) {
+						found[player._id] = true;
+						if(player.c != self.wid || player.n != self.members[player._id]){
+							if(player.n != self.members[player._id]){
+								console.log('Name change '+player.n+' -> '+self.members[player._id]);
+								player.n = self.members[player._id];
+							}
+							player.c = self.wid;
+							player.save(function(err) {
+								if(err){
+									console.log(err,"Player");
+								}
+							});
+						}
+					});
+					_(this.members).each(function(member, id) {
+						var id = parseInt(id, 10);
+						if(!found[id]){
+							var doc = new DBTypes.Player();
+							doc._id = id;
+							doc.c = self.wid;
+							doc.n = member;
+							console.log("New player created: "+member+"("+id+") (clan: "+self.wid+")");
+							doc.save(function(err){
+								if(err){
+									console.log(err,"Player");
+								}
+								else {
+									console.log("New player created: "+member+"("+id+") (clan: "+self.wid+")");
+								}
+							});
+						}
+					});
+				}
 			});
-			this.doc.ms = _.map(this.doc.ms,function(wid){
-				return parseInt(wid);
-			});
-			if(this.doc.ms && this.memberIds.length > 0){
-				_.each(this.doc.ms,function(wid){
-					if(!_.contains(self.memberIds,wid)){
-						var mc = new DBTypes.MemberChange({
-							c: self.wid,
-							p: wid,
-							ch: -1,
-							u: new Date()
-						});
-                        //self.saveMC(mc);
-						self.updatePlayer(wid,0);
-					}
-				});
-				_.each(this.memberIds,function(wid){
-					if(!_.contains(self.doc.ms,wid)){
-						var mc = new DBTypes.MemberChange({
-							c: self.wid,
-							p: wid,
-							ch: 1,
-							u: new Date()
-						});
-                        //self.saveMC(mc);
-						self.updatePlayer(wid,self.wid);
-					}
-				});
-			}else if(this.memberIds.length > 0){
-				_.each(self.memberIds,function(wid){
-					self.checkIfNew(wid);
-				});
-			}
-			this.doc.ms = this.memberIds;
+
+			this.doc.ms = ids;
 			this.doc.markModified('ms');
 		}
 		this.doc.u = new Date();
     	return true;
-  	},
-
-    saveMC: function(mc) {
-        DBTypes.MemberChange.findOne({c:mc.c, p:mc.p},function(err, doc){
-            if(doc){
-                var days = (mc.u.getTime() - doc.u.getTime())/(1000*3600*24);
-                if(days > 3){
-                    mc.save();
-                }else{
-                    console.log('Member change already exists for player:',mc.p);
-                }
-            }else{
-                mc.save();
-            }
-        });
-    },
-  	
-  	updatePlayer: function(wid,clan) {
-  		var self = this;
-  		DBTypes.Player.findOne({_id:wid},function(err, doc){
-			if(!doc){
-				doc = new DBTypes.Player();
-				doc._id = wid;
-				doc.s = "0";
-				doc.c = clan;
-				doc.save(function(err){
-					if(err)console.log(err,"Player");
-					else console.log("New player created: "+wid+" (clan: "+clan+")");
-				});
-			}else{
-				var prev = doc.c;
-				doc.c = clan;
-				doc.save(function(err){
-					if(err)console.log(err,"Player");
-					else console.log("Player updated: "+wid+" (clan: "+prev+"->"+clan+")");
-				});
-			}
-		});
   	},
 	
 	save: function(callback) {
